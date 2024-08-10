746cce20c6b939fa5bdcd26d3f583face8580724
Leonardo <leonardo.mandrade@stone.com.br>
2024-08-10 12:19:16 -0300

    insert operations on mongodb

--- Producer/CreateTransaction.cs
+++ Producer/CreateTransaction.cs
@@ -1,24 +1,29 @@
+using Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using System.Net;
 
 namespace Producer
 {
-    public class CreateTransaction(ILoggerFactory loggerFactory)
+    public class CreateTransaction(ILoggerFactory loggerFactory, IMongoDatabase mongoDatabase)
     {
         private readonly ILogger _logger = loggerFactory.CreateLogger<CreateTransaction>();
 
         [Function("CreateTransaction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            var transactions = mongoDatabase.GetCollection<Transaction>("operations");
 
+            await transactions.InsertOneAsync(new Transaction { Amount = (decimal)Random.Shared.NextDouble() });
+
+            await response.WriteStringAsync("Welcome to Azure Functions!");
             return response;
         }
     }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSingleton<IMongoClient, MongoClient>(provider =>
+        {
+            var connString = context.Configuration.GetConnectionString("MongoDB");
+            return new MongoClient(connString);
+        });
+        services.AddTransient(provider => provider.GetRequiredService<IMongoClient>().GetDatabase("transactions"));
+    })
     .Build();
 
-host.Run();
+await host.RunAsync();
